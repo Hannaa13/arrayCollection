519c69464dfd61a866c5202b17efacc834ea41a5
Anna Abuseridze <anna.abuseridze@gmail.com>
2019-10-22 11:40:05 +0300

    New Contains method

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -12,11 +12,12 @@ public class Main {
         System.out.println(Arrays.toString(arr.getArray()));
         System.out.println(Arrays.toString(arr.add("newElement")));
         System.out.println(arr.get(2));
-        System.out.println(arr.contains(8));
+        System.out.println(arr.containsForIndex(8));
         System.out.println(Arrays.toString(arr.clear()));
         System.out.println(arr.getSize());
         System.out.println(arr.find("a"));
         System.out.println(Arrays.toString(arr.delete("f")));
+        System.out.println(arr.contains("w"));
 
         Array arr2 = new Array();
         arr2.setArray(new String[]{"f", "e", "q", "a", "w"});
