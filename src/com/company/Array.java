0683fd475261c20707a34952da13432c25cc8e3a
Anna Abuseridze <anna.abuseridze@gmail.com>
2019-10-22 11:29:53 +0300

    Initial commit

---
+++ src/com/company/Array.java
@@ -0,0 +1,96 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Array {
+    private String[] array = new String[5];
+
+    public String[] getArray() {
+        return array;
+    }
+
+    public void setArray(String[] array) {
+        this.array = array;
+    }
+
+    public String[] add(String newElement) {
+
+        String[] arrayNew = new String[array.length + 1];
+        for (int i = 0; i < array.length; i++) {
+            arrayNew[i] = array[i];
+        }
+        arrayNew[array.length] = newElement;
+
+        return arrayNew;
+    }
+
+    public String get(int index) {
+        return array[index];
+    }
+
+
+    public boolean contains(int index) {
+
+        for (int i = 0; i < array.length; i++) {
+            if (index <= array.length) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public String[] clear() {
+        return new String[array.length];
+    }
+
+    public int getSize() {
+        return array.length;
+    }
+
+    public int find(String str) {
+        int index= 0;
+        for (int i = 0; i < array.length; i++) {
+            if (str.equals(array[i])) {
+               index = i;
+            }
+        }
+        return index;
+    }
+
+
+//    public boolean equals(Array arr) {
+//        if (this == arr) return true;
+//        if (arr == null || getClass() != arr.getClass()) return false;
+//        return Arrays.equals(array, ((Array) arr).array);
+//    }
+
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Array array1 = (Array) o;
+        return Arrays.equals(array, array1.array);
+    }
+
+    @Override
+    public int hashCode() {
+        return Arrays.hashCode(array);
+    }
+
+    public String[] delete(String delElem){
+        String[] arrayNew = new String[array.length - 1];
+        int index  = find(delElem);
+            for (int i = 0; i< index ; i++) {
+              arrayNew[i] = array[i];
+            }
+            for (int i = index; i<arrayNew.length ; i++) {
+                arrayNew[i] = array[i+1];
+
+            }
+            return arrayNew;
+
+    }
+
+
+}
---
+++ src/com/company/Main.java
@@ -0,0 +1,26 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Array arr = new Array();
+
+        arr.setArray(new String[]{"f", "e", "d", "a", "w"});
+        System.out.println(Arrays.toString(arr.getArray()));
+        System.out.println(Arrays.toString(arr.add("newElement")));
+        System.out.println(arr.get(2));
+        System.out.println(arr.contains(8));
+        System.out.println(Arrays.toString(arr.clear()));
+        System.out.println(arr.getSize());
+        System.out.println(arr.find("a"));
+        System.out.println(Arrays.toString(arr.delete("f")));
+
+        Array arr2 = new Array();
+        arr2.setArray(new String[]{"f", "e", "q", "a", "w"});
+        System.out.println(arr2.equals(arr));
+    }
+
+}
