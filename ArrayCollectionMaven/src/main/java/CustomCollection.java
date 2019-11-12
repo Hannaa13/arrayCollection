1e2c733e6678609f6f700822d90dac86c526b248
Anna Abuseridze <anna.abuseridze@gmail.com>
2019-11-12 22:49:52 +0200

    Tests

---
+++ ArrayCollectionMaven/src/main/java/CustomCollection.java
@@ -0,0 +1,9 @@
+import com.company.interfaces.impl.Collect;
+
+public class CustomCollection {
+
+    public static void main(String[] args) {
+        Collect arrayCollect = new Collect();
+        arrayCollect.add("hi");
+    }
+}
